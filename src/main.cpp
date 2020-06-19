a4619e47506c39a7171285c2da74a37df0b9585d
davidtazy <david.allemant@gmail.com>
2020-06-19 20:12:12 +0200

    init layout

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+
+    std::cout<<"hello tetris !!! "<<std::endl;
+    return 0;
+}
---
+++ test/test_game_logic.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch.hpp>
+
+
+TEST_CASE("first test"){
+    REQUIRE(false);
+}

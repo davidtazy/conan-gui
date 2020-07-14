898bc688130efe4638a10950f88449c3787f1028
davidtazy <david.allemant@gmail.com>
2020-07-14 20:12:48 +0200

    fix \r\n parse on windows

--- src/util.cpp
+++ src/util.cpp
@@ -3,12 +3,50 @@
 #include <vector>
 
 namespace util {
+
+std::istream& safeGetline(std::istream& is, std::string& t)
+{
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for(;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+        case '\n':
+            return is;
+        case '\r':
+            if(sb->sgetc() == '\n')
+                sb->sbumpc();
+            return is;
+        case std::streambuf::traits_type::eof():
+            // Also handle the case when the last line has no line ending
+            if(t.empty())
+                is.setstate(std::ios::eofbit);
+            return is;
+        default:
+            t += (char)c;
+        }
+    }
+}
+
+
+
 std::vector<std::string> split_line(const std::string& chaine) {
   std::vector<std::string> ret;
   std::stringstream ss(chaine);
   std::string sousChaine;
-  while (std::getline(ss, sousChaine)) {
-    ret.push_back(sousChaine);
+  while (safeGetline(ss, sousChaine)) {
+    if(sousChaine.size() > 0){
+      ret.push_back(sousChaine);
+    }
   }
   return ret;
 }

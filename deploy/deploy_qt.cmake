32b1a455f939508909259cb89577479123ed5e25
davidtazy <david.allemant@gmail.com>
2020-07-15 22:46:30 +0200

    create nsis installer on windows

--- deploy/deploy_qt.cmake
+++ deploy/deploy_qt.cmake
@@ -1,8 +1,8 @@
-message("deploy qt binaries to  ${CMAKE_INSTALL_PREFIX}")
+message("deploy qt binaries to  ${CMAKE_BINARY_DIR}/qt_deploy")
 
 find_package(PythonInterp REQUIRED)
 
 execute_process(COMMAND 
-                  ${PYTHON_EXECUTABLE} deploy.py ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_PREFIX}
+                  ${PYTHON_EXECUTABLE} deploy.py ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/qt_deploy
                 WORKING_DIRECTORY
                   ${CMAKE_CURRENT_LIST_DIR})
